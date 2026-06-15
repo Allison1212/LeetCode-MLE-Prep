5e9a3cb844b073a386879ef57253cd2a4ec56e4c
Allison1212 <jiaxueallison@outlook.com>
2026-06-14 17:56:16 -0700

    add 345

---
+++ String/345. Reverse Vowels of a String.py
@@ -0,0 +1,29 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        v_list= ['a','e','i','o','u']
+        s_list = list(s)
+        left = 0
+        right = len(s) - 1
+        left_w = s_list[left]
+        right_w = s_list[right]  
+
+        while right > left:
+            if left_w.lower() in v_list:
+                if right_w.lower() in v_list:
+                    s_list[left] = right_w
+                    s_list[right] = left_w
+                    right = right -1
+                    left = left + 1
+
+                    right_w = s_list[right]
+                    left_w = s_list[left] 
+                else:
+                    right = right - 1
+                    right_w = s_list[right] 
+            else:
+                left = left + 1
+                left_w = s_list[left]
+        return "".join(s_list)
+
+
+        
